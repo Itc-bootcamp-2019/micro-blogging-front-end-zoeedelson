b05fcd338b46bf626071d52362c98227801e3134
Zoe Edelson <zoeedelson@gmail.com>
2019-12-23 16:17:38 +0200

    context added

---
+++ src/components/pastPosts.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PublishedTweet from './publishedTweet.js';
+import MyContext from './MyContext.js';
+
+
+class PastPosts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    makePublishableTweet = (obj) => {
+        return (<PublishedTweet insideText={obj.content} date={obj.date} 
+            userName={obj.userName} 
+            />)
+    };
+    render(){
+        return(
+            <MyContext.Consumer>
+                {({addTweet, postData}) => (
+                <div>
+                    {postData.map((post) => this.makePublishableTweet(post))}
+                </div>
+                )}
+             </MyContext.Consumer>
+        )
+    }
+}
+
+export default PastPosts
--- src/components/postBox.js
+++ src/components/postBox.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './postBoxStylesheet.css';
 import PublishedTweet from './publishedTweet.js';
-import { getTweets, sendTweets } from './lib/api.js';
+import { getTweets, sendTweets } from '../lib/api.js';
+import PastPosts from './pastPosts.js';
+import MyContext from './MyContext.js';
+
 
 
 class PostBox extends React.Component {
@@ -14,6 +17,19 @@ class PostBox extends React.Component {
         };
         this.tweetText = this.tweetText.bind(this);
         this.makePublishableTweet = this.makePublishableTweet.bind(this);
+    }
+
+    addTweet = (newText,newDate, username) => {
+        let prevStatePostData = this.state.postData;        
+        this.setState({
+                    postData: [
+                        { content: newText, date: newDate, 
+                            userName: username 
+                        },
+                        ...prevStatePostData,
+                    ],
+                    isLoading: true,
+                })
     }
 
     componentDidMount() {
@@ -40,20 +56,10 @@ class PostBox extends React.Component {
     async sendToServer(e) {
         let newDate = new Date().toISOString();
         let newText = this.state.text;
-        let prevStatePostData = this.state.postData;
         let username = JSON.parse(localStorage.getItem('1'));
         await sendTweets({ content: newText, date: newDate, 
             userName: username 
-        }).then(() => 
-                this.setState({
-                    postData: [
-                        { content: newText, date: newDate, 
-                            userName: username 
-                        },
-                        ...prevStatePostData,
-                    ],
-                    isLoading: true,
-                }))
+        }).then(this.addTweet())
             .catch(() => alert("Issue sending to server :("),this.setState({isLoading:false}));
         await this.setState({
             text: '',
@@ -87,9 +93,13 @@ class PostBox extends React.Component {
                     {this.state.isLoading == true && JSON.parse(localStorage.getItem('1')) == null ?  (<textarea className="postBox postBoxWait" placeholder="Loading, please wait..." />) : (<textarea className="postBox" value={this.state.text} onChange={this.tweetText} placeholder="What's on your mind..." />)}
                     {JSON.parse(localStorage.getItem('1')) != null && this.state.isLoading == false && this.state.text.length < 140 ? (<button className="tweetButton" onClick={() => { this.sendToServer() }}>Tweet</button>) : (<button className="tweetButtonDisabled" >Tweet</button> )}
                 </div>
-                <div>
+                <MyContext.Provider value={this.state}>
+                    <PastPosts/>
+                </MyContext.Provider>
+
+                {/* <div>
                     {this.state.postData.map((post) => this.makePublishableTweet(post))}
-                </div>
+                </div> */}
             </div>
         )
     }
