96827a34cf12b9926385d2f2724d72e4b1b3856e
Zoe Edelson <zoeedelson@gmail.com>
2019-12-26 12:59:24 +0200

    navbar styling fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
 import "./navbarstylesheet.css";
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isHomePage: true
-    };
+    this.state = {};
   }
 
   render() {
@@ -14,22 +17,14 @@ class Navbar extends React.Component {
     return (
       <ul className="nav ">
         <li>
-          <Link
-            className={"pages " + (this.state.isHomePage ? "white" : "")}
-            onClick={() => this.setState({ isHomePage: true })}
-            to="/"
-          >
+          <NavLink className="pages" to="/" exact activeClassName="white">
             Home
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link
-            className={"pages " + (!this.state.isHomePage ? "white" : "")}
-            onClick={() => this.setState({ isHomePage: false })}
-            to="/username"
-          >
+          <NavLink className="pages" to="/username" activeClassName="white">
             Username
-          </Link>
+          </NavLink>
         </li>
       </ul>
     );
